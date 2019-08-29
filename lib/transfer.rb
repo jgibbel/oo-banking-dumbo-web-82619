5e97a59d0d24f0f48607cff615fd94a6e3bd11fe
James Gibbel <jamesgibbel@gmail.com>
2019-08-29 20:16:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,14 +11,14 @@ class Transfer
   end 
   
   def valid?
-    if @sender.valid? &&  @receiver.valid?
+    if @sender.valid? && @receiver.valid?
       true 
     else false 
     end 
   end 
   
   def execute_transaction
-    if (@status == "pending" && @sender.balance > @amount && @sender.valid?)
+    if @status == "pending" && @sender.balance > @amount && self.valid?
     @sender.balance -= @amount
     @receiver.balance += @amount
     @status = "complete"
