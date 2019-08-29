ea5179d033c9dfb5347f95f2e97a6b652a786b61
James Gibbel <jamesgibbel@gmail.com>
2019-08-29 20:17:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,9 @@ class Transfer
     @sender.balance -= @amount
     @receiver.balance += @amount
     @status = "complete"
-    else "Transaction rejected. Please check your account balance."
+    else 
+      "Transaction rejected. Please check your account balance."
+      @status = "rejected"
     end 
   end 
   
