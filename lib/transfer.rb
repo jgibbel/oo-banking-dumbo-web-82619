0d259fd6e71fa40134b12b1150adc28e677b5471
James Gibbel <jamesgibbel@gmail.com>
2019-08-29 20:19:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,6 +26,7 @@ class Transfer
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
+  end 
     
   def reverse_transfer
     if @status == "complete" && @receiver.balance > @amount && self.valid?
